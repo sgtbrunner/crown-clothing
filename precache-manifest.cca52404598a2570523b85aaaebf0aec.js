a88b0d423524835666061feedb8a687a43cdd964
Guilherme Brunner <guilherme.brunner@gmail.com>
2020-08-13 21:31:47 -0300

    Updates

---
+++ precache-manifest.cca52404598a2570523b85aaaebf0aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2005a59c5a7b4522d968d9cb4365ab8e",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "28586449686fefd8ac83",
+    "url": "/crown-clothing/static/css/main.feb4a9a8.chunk.css"
+  },
+  {
+    "revision": "6eacaf44daec12989e5f",
+    "url": "/crown-clothing/static/js/2.e6a28046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crown-clothing/static/js/2.e6a28046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28586449686fefd8ac83",
+    "url": "/crown-clothing/static/js/main.da35462e.chunk.js"
+  },
+  {
+    "revision": "961db21a70ed6e6fce39",
+    "url": "/crown-clothing/static/js/runtime-main.abaa3ce4.js"
+  }
+]);

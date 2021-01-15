8d4e105b8769c09248f18cf31b7b0a6a1c4f488a
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-01-14 21:07:34 -0300

    Replacing BrowserRouter with HashRouter | Setting up homepage in package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
@@ -11,7 +11,7 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router basename='/'>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
